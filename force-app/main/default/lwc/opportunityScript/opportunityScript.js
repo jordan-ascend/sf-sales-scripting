97c24a645bacde712103e04fa9c7957da440f5d7
Joshua Gaude <joshua@ascendsalesforceconsulting.com>
2021-01-29 17:22:32 -0600

    Script Display Controller and Test Work.

--- force-app/main/default/lwc/opportunityScript/opportunityScript.js
+++ force-app/main/default/lwc/opportunityScript/opportunityScript.js
@@ -1,15 +1,39 @@
 import { LightningElement, track, wire, api } from 'lwc';
 import getScript from '@salesforce/apex/ScriptDisplayController.getScript';
+import recordScriptUse from '@salesforce/apex/ScriptDisplayController.handleMetricObject';
+import buttonEnabled from '@salesforce/apex/ScriptDisplayController.shouldButtonBeDisabled';
 
 export default class OpportunityScript extends LightningElement {
 
     searchKey = '';
+    errorMsg = '';
+    buttonDisabledBool = true;
     @api recordId;
 
-    @wire(getScript, {OppId:'$searchKey'}) scriptObject;
+    @wire(getScript, {oppId:'$searchKey'}) scriptObject;
     error;
 
     connectedCallback(){
         this.searchKey = this.recordId;
+        
+        buttonEnabled({oppId:this.searchKey})
+        .then(result => {
+            this.buttonDisabledBool = result;
+            let readButton = this.template.querySelector('lightning-button');
+            readButton.disabled = this.buttonDisabledBool;
+        })
+        .catch(error =>{
+            this.error = error;
+        });
+    }
+
+    markRead(event) {
+        recordScriptUse({oppId:this.searchKey})
+        .then(result =>{
+            readButton.disabled = true;
+        })
+        .catch(error =>{
+            this.error = error;
+        });
     }
 }

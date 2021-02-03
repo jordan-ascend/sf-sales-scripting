72e93ca306a9b8285f014e768ed4c3db25af3ce6
Joshua Gaude <joshua@ascendsalesforceconsulting.com>
2021-02-03 17:47:38 -0600

    Created bar chart component for scripts with won opportunities and created junction object for tracking script use.

---
+++ force-app/main/default/lwc/opporunityWonScriptChart/opporunityWonScriptChart.js
@@ -0,0 +1,61 @@
+/**
+ * Copied from example for ChartJs. Will modify for our use.
+ * https://www.forcetrails.com/2020/04/bar-chart-in-lightning-web-component-lwc-bar-chartjs.html
+ */
+import {LightningElement, wire, track} from 'lwc';
+import getOpportunities from '@salesforce/apex/ScriptChartsController.getScriptsThatWin';
+export default class opporunityWonScriptChart extends LightningElement {
+    @track chartConfiguration;
+
+    @wire(getOpportunities, {})
+    getOpportunities({data, error}) {
+        if (error) {
+        this.error = error;
+        console.log('error => ' + JSON.stringify(error));
+        this.chartConfiguration = undefined;
+        } else if (data) {
+            console.log('data::');
+            console.log(data);
+            let chartData = [];
+            let chartLabels = [];
+            // for(let key in data) {
+            //     chartLabels.push(key);
+            //     chartData.push(data[key]);
+            // }
+
+            data.forEach(obj => {
+                chartData.push(obj.count);
+                chartLabels.push(obj.name);
+               });
+
+            this.chartConfiguration = {
+                type: 'bar',
+                data: {
+                    labels: chartLabels,
+                    datasets: [
+                        {
+                            label: 'Closed Won Last Week',
+                            barPercentage: 0.5,
+                            barThickness: 6,
+                            maxBarThickness: 8,
+                            minBarLength: 2,
+                            backgroundColor: "green",
+                            data: chartData,
+                        },
+                    ],
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                },
+            };
+            console.log('data => ', data);
+            this.error = undefined;
+        }
+    }
+}

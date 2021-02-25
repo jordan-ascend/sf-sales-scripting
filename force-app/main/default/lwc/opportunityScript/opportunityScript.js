e235ce8048218bfe65eb5fdc67644f2067fbae9e
Joshua Gaude <joshua@ascendsalesforceconsulting.com>
2021-02-25 17:36:05 -0600

    Script and metrics update when opportunity stage updates.

--- force-app/main/default/lwc/opportunityScript/opportunityScript.js
+++ force-app/main/default/lwc/opportunityScript/opportunityScript.js
@@ -2,10 +2,12 @@ import { LightningElement, track, wire, api } from 'lwc';
 import getScript from '@salesforce/apex/ScriptDisplayController.getScript';
 import recordScriptUse from '@salesforce/apex/ScriptDisplayController.handleMetricObject';
 import buttonEnabled from '@salesforce/apex/ScriptDisplayController.shouldButtonBeDisabled';
+import getOppStage from '@salesforce/apex/ScriptDisplayController.getOppStage';
 import { subscribe, unsubscribe, onError, setDebugFlag, isEmpEnabled }  from 'lightning/empApi';
-import getNewScript from '@salesforce/apex/ScriptDisplayController.getNewScript';
+// import getNewScript from '@salesforce/apex/ScriptDisplayController.getNewScript';
 import { NavigationMixin } from 'lightning/navigation';
 import { refreshApex } from '@salesforce/apex';
+import MailingPostalCode from '@salesforce/schema/Contact.MailingPostalCode';
 
 export default class OpportunityScript extends NavigationMixin(LightningElement) {
 
@@ -13,20 +15,57 @@ export default class OpportunityScript extends NavigationMixin(LightningElement)
     channelName = '/event/Opportunity_Listen__e';
     isSubscribed = false;
 
+    @track scriptMap;
+    @track lightningCardName = '';
+    @track scriptObject = '';
+
     // Tracks changes to channelName text field
     handleChannelName(event) {
         this.channelName = event.target.value;
     }
 
-    searchKey = '';
+    searchKey;
     errorMsg = '';
     buttonDisabledBool = true;
+    @track opportunityStage = '';
+
     @api recordId;
 
-    @wire(getScript, {oppId:'$searchKey'}) scriptObject;
+    @wire(getScript, {oppId:'$searchKey'}) 
+    wiredResult(data, error) { 
+        if(error) {
+            this.error = error;
+            console.log('error => ' + JSON.stringify(error));
+        }
+        if(data) {
+            console.log('data::Line40');
+            console.log(data.data);
+
+            // this.scriptMap = data.data;
+            for(let key in data.data) {
+                // Preventing unexcepted data
+                if (data.data.hasOwnProperty(key)) { // Filtering the data in the loop
+                    console.log('Key::' + key);
+                    console.log('value::'+data.data[key].Name);
+                    // this.scriptMap.push({value:data.data[key], key:key});
+                    if(key == this.opportunityStage) {
+                        this.scriptObject = data.data[key];
+                        this.lightningCardName = data.data[key].Name;
+                    }
+                }
+            }
+            if(typeof data.data !== 'undefined') {
+                console.log('scriptMap::');
+                this.scriptMap = data.data;
+                console.log(this.scriptMap['Prospecting'].Name);
+            }
+        }
+    }
+        //scriptMap;
     error;
 
     connectedCallback(){
+        // this.scriptMap = new Map();
         this.searchKey = this.recordId;
         
         buttonEnabled({oppId:this.searchKey})
@@ -42,23 +81,36 @@ export default class OpportunityScript extends NavigationMixin(LightningElement)
         if(this.isSubscribed == false) {
             this.handleSubscribe();
         }
-    }
-
-    markRead(event) {
-        recordScriptUse({oppId:this.searchKey})
-        .then(result =>{
-            readButton.disabled = true;
+        console.log('::Search key:::', this.recordId);
+        getOppStage({oppId:this.recordId})
+        .then(result => {
+            console.log('::getting stage name::');
+            this.opportunityStage = result;
+            console.log(this.opportunityStage);
         })
         .catch(error =>{
             this.error = error;
         });
     }
 
-    getNewScript(event) {
-        console.log('::Refresh::');
-        refreshApex(this.scriptObject);
-        this.connectedCallback()
-    }
+    // markRead(event) {
+    //     recordScriptUse({oppId:this.searchKey})
+    //     .then(result =>{
+    //         readButton.disabled = true;
+    //     })
+    //     .catch(error =>{
+    //         this.error = error;
+    //     });
+    // }
+
+    // getNewScript(event) {
+    //     console.log('::Refresh::');
+    //     console.log(this.scriptMap);
+    //     console.log(this.opportunityStage);
+
+    //     // refreshApex(this.scriptObject);
+    //     // this.connectedCallback()
+    // }
 
     goToScriptRecord(event) {
         // Navigate to the script editor page
@@ -66,7 +118,7 @@ export default class OpportunityScript extends NavigationMixin(LightningElement)
             type: 'standard__objectPage',
             attributes: {
                 objectApiName: 'Sales_Rep_Script__c',
-                recordId: this.scriptObject.data.Id,
+                recordId: this.scriptObject.Id,
                 actionName: 'edit',
             },
         });
@@ -75,15 +127,35 @@ export default class OpportunityScript extends NavigationMixin(LightningElement)
     handleSubscribe() {
         // Callback invoked whenever a new event message is received
         const messageCallback = function(response) {
+            
             console.log('New message received: ', JSON.stringify(response));
+            
+            console.log('::Record Id:::');
+            console.log(this.recordId);
+            
+            console.log('::Search key:::');
+            console.log(this.searchKey);
+            // console.log('Stage name: '+ JSON.parse(response));
             // Want to specifically see that event has fired successfully before refreshing apex.
             // var resObj = JSON.parse(response);
             // console.log('Parsed object: '+ resObj);
             // console.log('Extracted message: '+ resObj.data.payload.createdDate);
             console.log('Refreshing apex...');
-            refreshApex(this.scriptObject);
-            this.connectedCallback();
-        };
+            // refreshApex(this.scriptObject);
+            // this.connectedCallback();
+            getOppStage({oppId:this.searchKey})
+            .then(result => {
+                console.log('::getting stage name::');
+                this.opportunityStage = result;
+                if(typeof this.scriptMap !== 'undefined') {
+                    this.scriptObject = this.scriptMap[result];
+                }
+                console.log(this.opportunityStage);
+            })
+            .catch(error =>{
+                this.error = error;
+            });
+        }.bind(this);
 
         // Invoke subscribe method of empApi. Pass reference to messageCallback
         subscribe(this.channelName, -1, messageCallback).then(response => {
--- force-app/main/default/triggers/OpportunityTrigger.trigger
+++ force-app/main/default/triggers/OpportunityTrigger.trigger
@@ -1,11 +1,15 @@
 /**
  * Note to come back and see if we still need this trigger and related handler, OpportunityTriggerHandler.
  */
-trigger OpportunityTrigger on Opportunity (after insert, after update) {
+trigger OpportunityTrigger on Opportunity (before update, after insert, after update) {
     if(Trigger.isInsert) {
         OpportunityTriggerHandler.afterInsert(Trigger.newMap);
     }
     if(Trigger.isUpdate) {
-        OpportunityTriggerHandler.afterUpdate(Trigger.oldMap, Trigger.newMap);
+        if(Trigger.isBefore) {
+            // OpportunityTriggerHandler.breforeUpdate(Trigger.oldMap);
+        } else {
+            OpportunityTriggerHandler.afterUpdate(Trigger.oldMap, Trigger.newMap);
+        }
     }
 }

afef7e59deca8e60927d575070be410e58479368
Joshua Gaude <joshua@ascendsalesforceconsulting.com>
2021-03-12 15:57:48 -0600

    Edit button only visible to Script Admin and Script User.

--- force-app/main/default/lwc/opportunityScript/opportunityScript.js
+++ force-app/main/default/lwc/opportunityScript/opportunityScript.js
@@ -1,7 +1,8 @@
 import { LightningElement, track, wire, api } from 'lwc';
 import getScript from '@salesforce/apex/ScriptDisplayController.getScript';
 import recordScriptUse from '@salesforce/apex/MetricObjectHandler.handleMetricObject';
-import buttonEnabled from '@salesforce/apex/ScriptDisplayController.shouldButtonBeDisabled';
+// import buttonEnabled from '@salesforce/apex/ScriptDisplayController.shouldButtonBeDisabled';
+import editVisible from '@salesforce/apex/ScriptDisplayController.scriptUserCanEdit';
 import getOppStage from '@salesforce/apex/ScriptDisplayController.getOppStage';
 import { subscribe, unsubscribe, onError, setDebugFlag, isEmpEnabled }  from 'lightning/empApi';
 // import getNewScript from '@salesforce/apex/ScriptDisplayController.getNewScript';
@@ -14,6 +15,7 @@ export default class OpportunityScript extends NavigationMixin(LightningElement)
     subscription = {};
     channelName = '/event/Opportunity_Listen__e';
     isSubscribed = false;
+    showEditButton = false;
 
     @track scriptMap;
     @track lightningCardName = '';
@@ -68,15 +70,22 @@ export default class OpportunityScript extends NavigationMixin(LightningElement)
         // this.scriptMap = new Map();
         this.searchKey = this.recordId;
         
-        buttonEnabled({oppId:this.searchKey})
+        // buttonEnabled({oppId:this.searchKey})
+        // .then(result => {
+        //     this.buttonDisabledBool = result;
+        //     let readButton = this.template.querySelector('lightning-button');
+        //     readButton.disabled = this.buttonDisabledBool;
+        // })
+        // .catch(error =>{
+        //     this.error = error;
+        // });
+        editVisible()
         .then(result => {
-            this.buttonDisabledBool = result;
-            let readButton = this.template.querySelector('lightning-button');
-            readButton.disabled = this.buttonDisabledBool;
+            this.showEditButton = result;
         })
-        .catch(error =>{
+        .catch(error => {
             this.error = error;
-        });
+        })
 
         if(this.isSubscribed == false) {
             this.handleSubscribe();
